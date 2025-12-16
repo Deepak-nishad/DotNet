d1004cfcdb822a63435f8044aa2643b9baf250bf
Deepak <deepakkumar62559@gmail.com>
2025-12-16 11:00:50 +0530

    first commit

---
+++ BasicProgram/Second.cs
@@ -0,0 +1,11 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+  	int a=20;
+    int b=30;
+    int res=a+b;
+    Console.WriteLine("Addition of {0}+ {1}= {2}", a, b, res);
+   
+  }
+}
